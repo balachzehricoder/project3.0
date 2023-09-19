619cb3e19dd9703f2ec3e8851a0d2e5022b2f7b1
balachzehricoder <balachzehr7@gmail.com>
2023-09-19 15:23:14 +0500

    database

--- all databases/phonesell_com (1).sql
+++ all databases/phonesell_com (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 07, 2023 at 12:23 AM
+-- Generation Time: Sep 19, 2023 at 12:21 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -59,10 +59,10 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`id`, `name`, `picture`, `description`) VALUES
-(6, 'Realme', 'uploads/R.png', 'Realme is a very good brand'),
-(7, 'Samsung', 'uploads/OIP (3).jpeg', 'Samsung is a very good company '),
-(8, 'Apple', 'uploads/download.jpeg', 'apple is a good comany'),
-(9, 'Accessories', 'uploads/OIP.jpeg', 'a phone charger and may types of headphones and data cable');
+(23, 'realme', 'category_pictures/OIP.jpeg', 'realme is a good brand'),
+(24, 'samsung', 'category_pictures/OIP (1).jpeg', 'it is a very huge brand'),
+(25, 'apple', 'category_pictures/OIP (2).jpeg', 'make a good products '),
+(26, 'Accessory', 'category_pictures/OPI.png', 'threr are a many type of handsfree charger datacable');
 
 -- --------------------------------------------------------
 
@@ -84,22 +84,24 @@ CREATE TABLE `orders` (
 --
 
 INSERT INTO `orders` (`id`, `user_id`, `total`, `delivery_charges`, `order_date_time`, `status`) VALUES
-(214, 15, 76000, 200, '2023-08-30 03:30:11', 'delivered'),
-(215, 15, 75000, 200, '2023-08-30 03:31:32', 'pending'),
-(216, 15, 33000, 200, '2023-09-04 06:53:57', 'delivered'),
-(217, 15, 20000, 200, '2023-09-04 07:11:54', 'pending'),
-(218, 16, 33000, 200, '2023-09-04 10:32:12', 'pending'),
-(219, 15, 20000, 200, '2023-09-04 15:56:46', 'pending'),
-(220, 15, 100, 200, '2023-09-05 10:32:24', 'pending'),
-(221, 15, 35000, 200, '2023-09-05 10:32:53', 'pending'),
-(222, 15, 113000, 200, '2023-09-05 15:07:52', 'pending'),
-(223, 15, 21000, 200, '2023-09-06 11:56:51', 'pending'),
-(224, 15, 241080, 200, '2023-09-06 12:06:35', 'pending'),
-(225, 15, 1096512, 200, '2023-09-06 12:44:12', 'pending'),
-(226, 15, 35000, 200, '2023-09-06 12:45:39', 'pending'),
-(227, 15, 900120, 200, '2023-09-06 20:39:59', 'pending'),
-(228, 15, 728120, 200, '2023-09-06 20:40:49', 'pending'),
-(229, 15, 1275192, 200, '2023-09-06 20:42:51', 'pending');
+(231, 15, 106999, 200, '2023-09-14 22:17:40', 'delivered'),
+(232, 15, 30000, 200, '2023-09-14 22:18:53', 'delivered'),
+(233, 17, 3301977, 200, '2023-09-15 13:03:23', 'delivered'),
+(234, 15, 40000, 200, '2023-09-18 07:15:31', 'delivered'),
+(235, 17, 43000, 200, '2023-09-18 07:16:50', 'delivered'),
+(236, 15, 20000, 200, '2023-09-19 11:43:50', 'delivered'),
+(237, 15, 69000, 200, '2023-09-19 11:45:22', 'pending'),
+(238, 15, 46000, 200, '2023-09-19 11:45:55', 'pending'),
+(239, 15, 15000, 200, '2023-09-19 11:47:36', 'pending'),
+(240, 15, 23000, 200, '2023-09-19 11:49:01', 'delivered'),
+(241, 15, 15000, 200, '2023-09-19 11:49:44', 'pending'),
+(242, 15, 20000, 200, '2023-09-19 11:50:35', 'pending'),
+(243, 17, 20000, 200, '2023-09-19 12:03:57', 'pending'),
+(244, 15, 90000, 200, '2023-09-19 12:13:04', 'pending'),
+(245, 17, 20000, 200, '2023-09-19 12:14:40', 'pending'),
+(246, 17, 20000, 200, '2023-09-19 12:16:09', 'pending'),
+(247, 17, 20000, 200, '2023-09-19 12:16:40', 'pending'),
+(248, 17, 46000, 200, '2023-09-19 12:17:31', 'pending');
 
 -- --------------------------------------------------------
 
@@ -120,46 +122,28 @@ CREATE TABLE `order_details` (
 --
 
 INSERT INTO `order_details` (`id`, `order_id`, `product_id`, `price`, `qty`) VALUES
-(267, 214, 6, 20000, 1),
-(268, 214, 7, 35000, 1),
-(269, 214, 8, 21000, 1),
-(270, 215, 7, 35000, 1),
-(271, 215, 11, 40000, 1),
-(272, 216, 8, 21000, 1),
-(273, 216, 9, 12000, 1),
-(274, 217, 6, 20000, 1),
-(275, 218, 8, 21000, 1),
-(276, 218, 9, 12000, 1),
-(277, 219, 6, 20000, 1),
-(278, 220, 20, 100, 1),
-(279, 221, 7, 35000, 1),
-(280, 222, 9, 12000, 1),
-(281, 222, 8, 21000, 1),
-(282, 222, 11, 40000, 1),
-(283, 222, 6, 20000, 1),
-(284, 222, 10, 20000, 1),
-(285, 223, 8, 21000, 1),
-(286, 224, 8, 21000, 1),
-(287, 224, 10, 20000, 1),
-(288, 224, 13, 200080, 1),
-(289, 225, 17, 1000012, 1),
-(290, 225, 22, 6500, 1),
-(291, 225, 16, 90000, 1),
-(292, 226, 7, 35000, 1),
-(293, 227, 12, 300040, 3),
-(294, 228, 11, 40000, 1),
-(295, 228, 9, 12000, 1),
-(296, 228, 8, 21000, 1),
-(297, 228, 7, 35000, 1),
-(298, 228, 6, 20000, 1),
-(299, 228, 12, 300040, 1),
-(300, 228, 13, 200080, 1),
-(301, 228, 10, 20000, 5),
-(302, 229, 20, 100, 1),
-(303, 229, 17, 1000012, 1),
-(304, 229, 11, 40000, 1),
-(305, 229, 7, 35000, 1),
-(306, 229, 13, 200080, 1);
+(308, 231, 31, 23000, 1),
+(309, 231, 32, 15000, 1),
+(310, 231, 35, 68999, 1),
+(311, 232, 33, 30000, 1),
+(312, 233, 36, 138999, 23),
+(313, 233, 32, 15000, 7),
+(314, 234, 30, 20000, 2),
+(315, 235, 30, 20000, 1),
+(316, 235, 31, 23000, 1),
+(317, 236, 30, 20000, 1),
+(318, 237, 31, 23000, 3),
+(319, 238, 31, 23000, 2),
+(320, 239, 32, 15000, 1),
+(321, 240, 31, 23000, 1),
+(322, 241, 32, 15000, 1),
+(323, 242, 30, 20000, 1),
+(324, 243, 30, 20000, 1),
+(325, 244, 33, 30000, 3),
+(326, 245, 30, 20000, 1),
+(327, 246, 30, 20000, 1),
+(328, 247, 30, 20000, 1),
+(329, 248, 31, 23000, 2);
 
 -- --------------------------------------------------------
 
@@ -182,27 +166,13 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `p_name`, `p_price`, `p_qty`, `p_tax`, `img_upload`, `category_id`) VALUES
-(6, ' c2', 20000, 1, 1, 'databaseimgs/OIP (3).jpeg', 6),
-(7, 'realme c33', 35000, 1, 1, 'databaseimgs/OIP (9).jpeg', 6),
-(8, 'realme c21', 21000, 1, 1, 'databaseimgs/OIP (7).jpeg', 6),
-(9, 'realme c 12', 12000, 1, 1, 'databaseimgs/OIP (6).jpeg', 6),
-(10, 'realme c3', 20000, 1, 1, 'databaseimgs/OIP (5).jpeg', 6),
-(11, 'iphone 8', 40000, 1, 1, 'databaseimgs/OIP (13).jpeg', 8),
-(12, 'iphone 14', 300040, 1, 1, 'databaseimgs/download (1).jpeg', 8),
-(13, 'iphone 13', 200080, 1, 1, 'databaseimgs/OIP (14).jpeg', 8),
-(14, 'iphone 12', 100040, 1, 1, 'databaseimgs/OIP (16).jpeg', 8),
-(15, 'iphone 11', 100000, 1, 1, 'databaseimgs/OIP (15).jpeg', 8),
-(16, 'iphone x', 90000, 1, 1, 'databaseimgs/download.jpeg', 8),
-(17, 'Samsung a33', 1000012, 1, 1, 'databaseimgs/OIP (11).jpeg', 7),
-(18, 'smasung a34', 1000040, 1, 1, 'databaseimgs/OIP (12).jpeg', 7),
-(19, 'realme data cable', 300, 1, 1, 'databaseimgs/OIP (17).jpeg', 9),
-(20, 'realme handsfree', 100, 1, 1, 'databaseimgs/OIP (18).jpeg', 9),
-(22, 'realme air burd', 6500, 1, 1, 'databaseimgs/OIP (19).jpeg', 9),
-(23, 'Samsung adapter', 500, 1, 1, 'databaseimgs/OIP (22).jpeg', 9),
-(24, 'Samsung akg handfree', 600, 1, 1, 'databaseimgs/OIP (21).jpeg', 9),
-(25, 'apple data cable', 800, 1, 1, 'databaseimgs/OIP (23).jpeg', 9),
-(26, 'apple handsfree', 700, 1, 1, 'databaseimgs/OIP (24).jpeg', 9),
-(27, 'apple converter', 300, 1, 1, 'databaseimgs/OIP (25).jpeg', 9);
+(30, 'realme c2', 20000, 1, 12, 'databaseimgs/OIP (3).jpeg', 23),
+(31, 'realme c3', 23000, 1, 13, 'databaseimgs/OIP (5).jpeg', 23),
+(32, 'realme c12', 15000, 1, 10, 'databaseimgs/OIP (6).jpeg', 23),
+(33, 'realme c21', 30000, 1, 12, 'databaseimgs/OIP (7).jpeg', 23),
+(34, 'realme c33', 60000, 1, 12, 'databaseimgs/OIP (9).jpeg', 23),
+(35, 'Samsung galaxy a33', 68999, 1, 12, 'databaseimgs/OIP (12).jpeg', 24),
+(36, 'Samsung galaxy a34 ', 138999, 1, 12, 'databaseimgs/OIP (11).jpeg', 24);
 
 -- --------------------------------------------------------
 
@@ -226,7 +196,8 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `full_name`, `email`, `phone`, `password`, `role`, `address`) VALUES
 (15, 'balach', 'balachzehr@gmail.com', '2147483647', '$2y$10$vgpesEzlzONW8I9tnCVLGemsKKSi/iBg4vUlLdsEI8z3ifDpOi1lK', 'user', 'apsara appartment'),
-(16, 'hacker', 'hacker@gmail.com', '123', '$2y$10$v4SDAW5XY.ohRL9HV0xsO.PLDec1.txup.kmQTcw1XqbF7NJnHKC6', 'user', 'dhhdhdhd');
+(16, 'hacker', 'hacker@gmail.com', '123', '$2y$10$v4SDAW5XY.ohRL9HV0xsO.PLDec1.txup.kmQTcw1XqbF7NJnHKC6', 'user', 'dhhdhdhd'),
+(17, 'balach', 'balachzehr7@gmail.com', '03313345084', '$2y$10$k3pZauhpodZufOBt7OF5VOfbROzkzYUStfUSrpX5q0vAC5Na9HuuO', 'user', 'apsara apartment Gulshan-e-Iqbal block 17 karachi');
 
 -- --------------------------------------------------------
 
@@ -239,6 +210,13 @@ CREATE TABLE `wishlist` (
   `user_id` int(11) DEFAULT NULL,
   `product_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `wishlist`
+--
+
+INSERT INTO `wishlist` (`wishlist_id`, `user_id`, `product_id`) VALUES
+(19, 17, 32);
 
 --
 -- Indexes for dumped tables
@@ -307,37 +285,37 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=230;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=249;
 
 --
 -- AUTO_INCREMENT for table `order_details`
 --
 ALTER TABLE `order_details`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=307;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=330;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `wishlist`
 --
 ALTER TABLE `wishlist`
-  MODIFY `wishlist_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `wishlist_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- Constraints for dumped tables
